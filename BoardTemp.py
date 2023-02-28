f648771a2295b151abbf675529fef30d7a2c04e6
EG-L <81463782+EG-L@users.noreply.github.com>
2023-02-28 16:58:11 +0900

    Update BoardTemp.py

--- BoardTemp.py
+++ BoardTemp.py
@@ -7,18 +7,19 @@ class Board_temp:
         self.i2c = I2C(0,sda=Pin(20),scl=Pin(21),freq=400000)
         self.TEMP = 'b'
         self.devices = self.i2c.scan()
-        print(self.devices)
         
     def read_temp(self):
         while True:
             try:
-#                 print(self.i2c.readfrom(self.devices[0],1))
-                print('%s:%.1f'%('Celsius',struct.unpack(self.TEMP,self.i2c.readfrom(self.devices[0],1))[0]))
-                print('%s:%.1f'%('Fahrenheit',struct.unpack(self.TEMP,self.i2c.readfrom(self.devices[0],1))[0]*9.0/5.0+32.0))
-                utime.sleep(1)
+                TEMPDAT = struct.unpack(self.TEMP,self.i2c.readfrom(self.devices[0],1))[0]
+                print('%s:%.1f'%('Celsius',TEMPDAT))
+                utime.sleep(0.1)
             except:
+                TEMPDAT = 0
                 print('%s:%f'%('Celsius',0.0))
-                utime.sleep(1)
+                utime.sleep(0.1)
+            break
+        return TEMPDAT
 
-Board_temp().read_temp()
-                
+if __name__=='__main__':
+    Board_temp().read_temp()            
