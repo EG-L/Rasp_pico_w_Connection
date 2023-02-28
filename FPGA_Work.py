fa48c2f7dbdbde410a733516804a49b9e154dbe6
EG-L <81463782+EG-L@users.noreply.github.com>
2023-02-28 16:57:41 +0900

    Update FPGA_Work.py

--- FPGA_Work.py
+++ FPGA_Work.py
@@ -1,14 +1,23 @@
 from machine import Pin
 import utime
 
-while True:
-    pin = Pin(9,mode=Pin.IN)
-    try:
-        if pin.value() == 0:
-            print('Close')
-        else:
-            print('Open')
-    except:
-        print('Error')
-    utime.sleep(0.1)
+class FPGA_:
+    def __init__(self):
+        self.pin = 0
+        
+    def FPGA_Check(self,Working_set=Pin(9,mode=Pin.IN)):
+        self.pin = Working_set
+        try:
+            if self.pin.value() == 0:
+                print('Close')
+                return 0
+            else:
+                print('Open')
+                return 1
+        except Exception as e:
+            print('Error')
+            return 2
+        utime.sleep(0.1)
 
+if __name__=='__main__':
+    print(FPGA_().FPGA_Check())
