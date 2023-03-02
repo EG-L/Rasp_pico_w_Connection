66aecf25605032970d1655163bdeaf1e197d36b8
EG-L <81463782+EG-L@users.noreply.github.com>
2023-03-02 16:59:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from BoardTemp import Board_temp as Bt
 from FPGA_Work import FPGA_
 from UART import Uart_CONN as Conn
+from ADC import MUX
+import time
 
+err_check = 0
+
+DSPEN,TRXPSEN,TRXCSEN,DC12VMNT,MNTIN,err_check = MUX().MUX_CHECK(limit=0,Err_check=err_check)
 BoardTemp = Bt().read_temp() # 0 : error Data
 Working_check = FPGA_().FPGA_Check() # 0 : close , 1 : open, 2 : error
 ENVTEMP,Pressure = Conn().RS485READ() # 0 : error Data
 
-Conn().RS232WRITE(BDTEMP=BoardTemp,ENVTEMP=ENVTEMP,PRESSURE=Pressure,FPGAWORKING=Working_check)
+Conn().RS232WRITE(DSPEN=DSPEN,TRXEN=TRXPSEN,TRXCSEN=TRXCSEN,DC12VMNT=DC12VMNT,MNTIN=MNTIN,BDTEMP=BoardTemp,ENVTEMP=ENVTEMP,PRESSURE=Pressure,FPGAWORKING=Working_check)
 
